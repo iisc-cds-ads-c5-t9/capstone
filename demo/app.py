0e57a506d149ecdde0593d1492dd17a113ab1f41
Balaji Ganesan <balajinix@gmail.com>
2023-10-22 08:22:09 +0530

    pytorch version

--- demo/app.py
+++ demo/app.py
@@ -5,11 +5,20 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
+# Load your Siamese network model and other necessary libraries here
+import torch
+import torchtext
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader, TensorDataset
+
 # Load the questions and answers from the JSON file
 def load_questions_from_json(json_file):
     with open(json_file, 'r') as file:
         data = json.load(file)
     return random.sample(data, 10)  # Select 10 random samples
+
+def get_embedding(text):
+    return model.encode([text])[0]
 
 def get_random_samples(data, sample_size=10):
     return random.sample(data, sample_size)
@@ -23,12 +32,27 @@ def modify_question_with_option(question, option):
     return question + " " + option, None
 
 # Initialize the SBERT model
-model = SentenceTransformer('paraphrase-distilroberta-base-v1')
+sbert_model = SentenceTransformer('paraphrase-distilroberta-base-v1')
 
-def get_embedding(text):
-    return model.encode([text])[0]
+# Initialize your Siamese network model here (replace with your model)
+# siamese_model = YourSiameseNetwork()
 
-def compute_similarity(text1, text2):
+def get_embedding_sbert(text):
+    return sbert_model.encode([text])[0]
+
+# Function to compute similarity using your Siamese network
+def compute_similarity_siamese(text1, text2):
+    # Preprocess the text data (tokenization, padding, etc.) using your Siamese model
+    # Replace this with your actual preprocessing code
+    processed_text1 = preprocess_text_with_siamese(text1)
+    processed_text2 = preprocess_text_with_siamese(text2)
+    
+    # Make predictions using your Siamese model
+    with torch.no_grad():
+        similarity_score = siamese_model(processed_text1, processed_text2)
+    return similarity_score
+
+def compute_similarity_sbert(text1, text2):
     emb1 = get_embedding(text1)
     emb2 = get_embedding(text2)
     emb1_2d = emb1.reshape(1, -1)
@@ -68,31 +92,50 @@ def main():
                 current_question["distractor3"]
             ]
 
-        max_similarity = 0
-        predicted_option = ""
+        max_similarity_sbert = 0
+        predicted_option_sbert = ""
+        max_similarity_siamese = 0
+        predicted_option_siamese = ""
+        
         for option in options:
             modified_option, used_option = modify_question_with_option(current_question["question"], option)
-            similarity_score = compute_similarity(modified_option, support_text) * 100  # Convert to percentage
+            
+            # Compute similarity using SBERT
+            similarity_score_sbert = compute_similarity_sbert(modified_option, support_text) * 100
+            
+            # Compute similarity using your Siamese network
+            similarity_score_siamese = compute_similarity_siamese(modified_option, support_text) * 100
 
             # Highlight the option in bold if it's used in the modified question
             display_option = f"**{option}**" if option == used_option else option
-            st.write(f"Similarity between '{modified_option}' and Supporting Text: {similarity_score:.2f}% for option {display_option}")
+            st.write(f"Similarity (SBERT): {similarity_score_sbert:.2f}% for option {display_option}")
+            st.write(f"Similarity (Siamese): {similarity_score_siamese:.2f}% for option {display_option}")
 
-            # Track the option with the highest similarity score
-            if similarity_score > max_similarity:
-                max_similarity = similarity_score
-                predicted_option = option
+            # Track the option with the highest similarity score for both methods
+            if similarity_score_sbert > max_similarity_sbert:
+                max_similarity_sbert = similarity_score_sbert
+                predicted_option_sbert = option
+            
+            if similarity_score_siamese > max_similarity_siamese:
+                max_similarity_siamese = similarity_score_siamese
+                predicted_option_siamese = option
 
-        # Display the correct and predicted answers
+        # Display the correct and predicted answers for both methods
         correct_answer = current_question["correct_answer"]
         st.write("Correct Answer:", correct_answer)
-        st.write("Predicted Answer:", predicted_option)
+        st.write("Predicted Answer (SBERT):", predicted_option_sbert)
+        st.write("Predicted Answer (Siamese):", predicted_option_siamese)
         
-        # Check if the prediction is correct and display the result
-        if correct_answer == predicted_option:
-            st.markdown('<span style="color:green">Correct!</span>', unsafe_allow_html=True)
+        # Check if the predictions are correct and display the result for both methods
+        if correct_answer == predicted_option_sbert:
+            st.markdown('<span style="color:green">Correct! (SBERT)</span>', unsafe_allow_html=True)
         else:
-            st.markdown('<span style="color:red">Wrong!</span>', unsafe_allow_html=True)
+            st.markdown('<span style="color:red">Wrong! (SBERT)</span>', unsafe_allow_html=True)
+        
+        if correct_answer == predicted_option_siamese:
+            st.markdown('<span style="color:green">Correct! (Siamese)</span>', unsafe_allow_html=True)
+        else:
+            st.markdown('<span style="color:red">Wrong! (Siamese)</span>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
